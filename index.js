da3df934672061ea5ca82484ae8cd6ceafad5be5
Sivan Mehta <Sivanmehta2@gmail.com>
2019-01-14 21:04:44 -0800

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from S3!')
+    body: JSON.stringify('Hello from AWS S3!')
   };
 
   return response;
