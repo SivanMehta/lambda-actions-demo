efb2f86d43d4003e5655fc0cf2d23909ac3195c7
Sivan Mehta <Sivanmehta2@gmail.com>
2020-12-07 18:44:41 -0800

    fancy renaming

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ exports.handler = async (event) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Hello there!'
+      message: 'Hi there!',
+      query: event.queryStringParameters
     })
   };
 
